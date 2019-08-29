c4ef59f8c4db6c54e4fd92b6e97d08f31839c85a
alumno <alumno@LAB00PC00>
2019-08-29 20:09:03 -0300

    copie el codigo del profe

---
+++ clase04/src/clase04.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : clase04.c
+ Author      : lore
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+
+#define EXIT_ERROR -1
+
+int main(void)
+{
+	int resultado;
+	if(getInt(&resultado,"Edad?\n","Error\n",0,150,2)==0)
+	{
+		printf("El resultado es: %d",resultado);
+	}
+	return EXIT_SUCCESS;
+}
+
