baf645dcb7fd6aaa1901ab826b089cf68f59b65a
alumno <alumno@LAB00PC00>
2019-08-29 21:53:30 -0300

    copie clase 03

---
+++ clase3/src/clase3.c
@@ -0,0 +1,64 @@
+/*
+ ============================================================================
+ Name        : clase3.c
+ Author      : lore
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculaMaximoMinimoPromedio(int *maximo, int *minimo, float *promedio, int cantidad);
+
+int main(void) {
+	int maximo;
+	int minimo;
+	float promedio;
+	int cantidad;
+
+	printf("Ingrese la cantidad de casos ");
+	scanf("%d", &cantidad);
+
+	if(calculaMaximoMinimoPromedio(&maximo, &minimo, &promedio, cantidad) == 0){
+		printf("El maximo es %d el minimo es %d el promedio es %f", maximo, minimo, promedio);
+	}
+	return EXIT_SUCCESS;
+}
+
+int calculaMaximoMinimoPromedio(int *maximo, int *minimo, float *promedio, int cantidad){
+	int retorno = -1;
+	int flag = 0;
+	int numero;
+	int i;
+	int suma = 0;
+	int maximoTemp;
+	int minimoTemp;
+
+	for(i = 0; i < cantidad; i++){
+		printf("Ingrese un numero\n");
+		scanf("%d", &numero);
+
+		suma += numero;
+
+		if(flag == 0){
+			flag = 1;
+			maximoTemp = numero;
+			minimoTemp = numero;
+		}else if(numero > maximoTemp){
+			maximoTemp = numero;
+		}else if(numero < minimoTemp){
+			minimoTemp = numero;
+		}
+
+	}
+
+	*maximo = maximoTemp;
+	*minimo = minimoTemp;
+	*promedio = (float)suma / cantidad;
+	retorno = 0;
+
+	return retorno;
+}
